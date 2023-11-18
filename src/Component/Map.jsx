0bdbe10efa2ddceaa79793ba36bd5cfaaab31026
araraya <aryadanihlesmana@gmail.com>
2023-11-18 19:40:06 +0700

    (feature)get user location

--- src/Component/Map.jsx
+++ src/Component/Map.jsx
@@ -1,16 +1,59 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import {
   MapContainer,
   TileLayer,
   LayersControl,
+  useMap,
+  Marker,
+  Popup
 } from "react-leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-shadow.png",
+});
 
 const { BaseLayer } = LayersControl;
 
+function LocateUser(){
+    const [position, setPosition] = useState(null);
+    const map = useMap();
+
+    useEffect(() => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius, {
+            color: '#fc4332',
+            stroke: false,
+            opacity: 0.3
+        });
+        circle.addTo(map);
+      });
+    }, [map]);
+
+    return position === null ? null : (
+        <Marker position={position}>
+          <Popup>
+            You are here. <br />
+            Your latitude is {position.lat} <br/>
+            Your longitude is {position.lng}
+          </Popup>
+        </Marker>
+      );
+}
+
 function leafletMap(){
     return (
-        <MapContainer
+    <MapContainer
           center={[-6.3343, 106.8373]}
           zoom={10}
           scrollWheelZoom={true}
@@ -24,7 +67,7 @@ function leafletMap(){
           <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
         </BaseLayer>
       </LayersControl>
-    
+      <LocateUser/>
     </MapContainer>
   );
 }
