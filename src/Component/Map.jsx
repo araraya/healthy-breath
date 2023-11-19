af68f390779423dd5d39bae03a432fdc9da0927c
araraya <aryadanihlesmana@gmail.com>
2023-11-19 13:36:27 +0700

    (feature)get aqi based on mapBound

--- src/Component/Map.jsx
+++ src/Component/Map.jsx
@@ -10,6 +10,8 @@ import {
   Popup,
 } from "react-leaflet";
 import aqi from "../Service/aqi";
+import * as turf from "@turf/turf";
+
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
@@ -22,37 +24,7 @@ L.Icon.Default.mergeOptions({
 
 const { BaseLayer } = LayersControl;
 
-function LocateUser() {
-  const [position, setPosition] = useState(null);
-  const map = useMap();
-
-  useEffect(() => {
-    map.locate().on("locationfound", function (e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-      const radius = e.accuracy;
-      const circle = L.circle(e.latlng, radius, {
-        color: "#fc4332",
-        stroke: false,
-        opacity: 0.3,
-      });
-      circle.addTo(map);
-    });
-  }, [map]);
-  aqi();
-
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>
-        You are here. <br />
-        Your latitude is {position.lat} <br />
-        Your longitude is {position.lng}
-      </Popup>
-    </Marker>
-  );
-}
-
-function leafletMap() {
+function LeafletMap() {
   return (
     <MapContainer
       center={[-6.3343, 106.8373]}
@@ -68,9 +40,79 @@ function leafletMap() {
           <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
         </BaseLayer>
       </LayersControl>
-      <LocateUser />
+
+      <UserMarker />
     </MapContainer>
   );
 }
 
-export default leafletMap;
+function UserMarker() {
+  const [position, setPosition] = useState(null);
+  const [randomPoints, setRandomPoints] = useState([]);
+  const map = useMap();
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      console.log(e);
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+      const mapBounds = map.getBounds();
+      console.log(mapBounds);
+      const currentBbox = [
+        mapBounds.getNorthEast().lat,
+        mapBounds.getSouthWest().lng,
+        mapBounds.getSouthWest().lat,
+        mapBounds.getNorthEast().lng,
+      ];
+      console.log(currentBbox);
+      const randomPointsBbox = turf.randomPoint(25, { bbox: currentBbox });
+      const positionPoint = turf.point([e.latlng.lng, e.latlng.lat]);
+      console.log(positionPoint);
+      console.log(randomPointsBbox);
+      setRandomPoints((r) => [...randomPoints, ...randomPointsBbox.features]);
+      console.log(randomPoints);
+      const radius = e.accuracy;
+      const circle = L.circle(e.latlng, radius, {
+        color: "#fc4332",
+        stroke: false,
+        opacity: 0.3,
+      });
+      circle.addTo(map);
+
+      const allCoordinates = [...randomPointsBbox.features];
+      allCoordinates.push(positionPoint);
+      console.log(allCoordinates);
+      aqi(allCoordinates);
+    });
+  }, [map]);
+  if (position && randomPoints) {
+    return (
+      <>
+        <Marker position={position}>
+          <Popup>
+            You are here. <br />
+            Your latitude is {position.lat} <br />
+            Your longitude is {position.lng}
+          </Popup>
+        </Marker>
+
+        <RandomPoints randomPoints={randomPoints} />
+      </>
+    );
+  }
+}
+
+function RandomPoints({ randomPoints }) {
+  return randomPoints.map((rand, i) => {
+    const latlng = {
+      lat: rand.geometry.coordinates[0],
+      lng: rand.geometry.coordinates[1],
+    };
+    const id = i + 1;
+    return (
+      <Marker position={latlng} key={id}>
+        {" "}
+      </Marker>
+    );
+  });
+}
+export default LeafletMap;
--- src/Service/aqi.js
+++ src/Service/aqi.js
@@ -1,19 +1,24 @@
 import axios from "axios";
-async function GetAQI() {
+async function GetAQI(coordinates) {
     const apiKey = process.env.REACT_APP_AQI_API_KEY;
-    axios
-        .get(
-            `http://api.openweathermap.org/data/2.5/air_pollution?lat=-6.2160896&lon=106.9613056&appid=` +
-            `${apiKey}`
-        )
-        .then(function (response) {
-            // handle success
-            console.log(response);
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        });
+
+
+    coordinates.forEach(coordinate => {
+        axios
+            .get(
+                `http://api.openweathermap.org/data/2.5/air_pollution?lat=-6.2160896&lon=106.9613056&appid=` +
+                `${apiKey}`
+            )
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                coordinate.properties.aqi = response.data.list[0].main.aqi;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    })
 }
 
 export default GetAQI;
